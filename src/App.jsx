aa470bbd849ffa747a730b28fe2d0c4b9b8f4388
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-11-02 15:41:14 +0200

    Fixing Portfolio File name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import Home from './Components/home/Home.jsx';
 import Navbar from "./Components/navbar/Navbar.jsx";
 import Hero from "./Components/hero/Hero.jsx";
-import Portfolio from "./Components/Portfolio/portfolio.jsx";
+import Portfolio from "./Components/portfolio/Portfolio.jsx";
 import About from "./Components/about/About.jsx";
 import Contact from "./Components/contact/Contact.jsx";
 import Footer from "./Components/footer/Footer.jsx"
@@ -16,7 +16,7 @@ function App(){
     <Routes>
       <Route path= "/Home" element={<Home/>}></Route>
       <Route path= "/Hero" element={<Hero/>}></Route>
-      <Route path= "/Portfolio" element={<Portfolio/>}></Route>
+      <Route path= "/Portfolio" element={<portfolio/>}></Route>
       <Route path= "/About" element={<About/>}></Route>
       <Route path= "/Contact" element={<Contact/>}></Route>
     </Routes>
--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,5 +1,5 @@
 import Hero from "../hero/Hero.jsx";
-import Portfolio from "../Portfolio/portfolio.jsx";
+import Portfolio from "../portfolio/Portfolio.jsx";
 import About from "../about/About.jsx";
 import Contact from "../contact/Contact.jsx";
 
