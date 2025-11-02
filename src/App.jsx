c533aa93745c9c455e2cf7a82a59e11fd3f4eb8e
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-11-02 17:06:27 +0200

    Temp Names

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Fragment } from "react";
 import Navbar from "./Components/Navbar/Navbar.jsx";
-import Hero from "./Components/Hero/Hero.jsx";
-import About from "./Components/About/About.jsx";
-import Contact from "./Components/Contact/Contact.jsx";
-import Footer from "./Components/Footer/Footer.jsx"
+import Hero from "./Components/Hero/ero.jsx";
+import About from "./Components/About/bout.jsx";
+import Contact from "./Components/Contact/ontact.jsx";
+import Footer from "./Components/Footer/ooter.jsx"
 
-import Portfolio from "./Components/Portfolio/Portfolio.jsx";
+import Portfolio from "./Components/Portfolio/ortfolio.jsx";
 
 
 import { Route, Routes } from "react-router-dom";
